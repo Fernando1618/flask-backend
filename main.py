6cd3bc6ff2749dd3c22bda5653d4594436a043a8
FERS_432hz <fercho_7121@hotmail.com>
2025-07-15 14:50:11 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify({"message": "API funcionando correctamente"}), 200
+
+@app.route("/api/mensaje", methods=["POST"])
+def mensaje():
+    data = request.get_json()
+    mensaje = data.get("mensaje", "")
+    return jsonify({"respuesta": f"Mensaje recibido: {mensaje}"}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
