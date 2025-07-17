0d1a5c94ac5f24901d2b0a75dd0bfcf6a56932d2
FERS_432hz <fercho_7121@hotmail.com>
2025-07-16 23:40:52 -0600

    main.py

--- main.py
+++ main.py
@@ -2,30 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Comando global que guarda el último enviado por el HMI
-ultimo_comando = ""
-
 @app.route("/", methods=["GET"])
 def index():
     return jsonify({"message": "API funcionando correctamente"}), 200
 
-@app.route("/api/comando", methods=["POST"])
-def recibir_comando():
-    global ultimo_comando
+@app.route("/api/mensaje", methods=["POST"])
+def mensaje():
     data = request.get_json()
-    comando = data.get("comando", "").strip()
-    if comando:
-        ultimo_comando = comando
-        return jsonify({"status": "ok", "mensaje": f"Comando recibido: {comando}"}), 200
-    else:
-        return jsonify({"status": "error", "mensaje": "Comando vacío"}), 400
-
-@app.route("/api/comando-pendiente", methods=["GET"])
-def enviar_comando():
-    global ultimo_comando
-    comando = ultimo_comando
-    ultimo_comando = ""  # Limpiar después de entregar al ESP32
-    return jsonify({"comando": comando}), 200
+    mensaje = data.get("mensaje", "")
+    return jsonify({"respuesta": f"Mensaje recibido: {mensaje}"}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
